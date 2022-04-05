f4dd4eda85e650a3d263581dcc6d8e9459831bf0
Emanuel Poggio <emanuel.poggio@hotmail.com>
2022-04-05 10:37:46 -0300

    Calculadora Poggio E.M

---
+++ src/test/java/junit/epoggio/BuyingACarTest.java
@@ -0,0 +1,19 @@
+package junit.epoggio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuyingACarTest {
+        @Test
+        public void test1() {
+            int[] r = new int[] { 6, 766 };
+            assertArrayEquals(r, BuyingACar.BCMonths(2000, 8000, 1000, 1.5));
+        }
+        @Test
+        public void test2() {
+            int[] r = new int[] { 0, 4000 };
+            assertArrayEquals(r, BuyingACar.BCMonths(12000, 8000, 1000, 1.5));
+        }
+    }
+
