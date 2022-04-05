4a89b4ec7cb389cfd45c73245300bfd2e77f0941
Emanuel Poggio <emanuel.poggio@hotmail.com>
2022-04-05 15:18:00 -0300

    Poggio EM

--- src/test/java/junit/epoggio/Asserts.java
+++ src/test/java/junit/epoggio/Asserts.java
@@ -2,6 +2,9 @@ package junit.epoggio;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class Asserts {
@@ -19,12 +22,38 @@ public class Asserts {
     public void cadenas(){
         String s1 = "Bootcamp";
         String s2 = "TSoft";
-        assertEquals(s1,s2);
+       // assertEquals(s1,s2);
         assertNotEquals(s1,s2);
+        // para strings
     }
     @Test
     public void arrays (){
-        String[] array = {"a","b","c"};
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        ArrayList<String> arrayList2 = new ArrayList<>(); //declaramos los array como objetos.
+
+        for (int i = 0; i < 10 ; i++) { //llenamos el array
+            String elementoArray = "" +(i+1);
+            arrayList1.add(elementoArray);
+            arrayList2.add(elementoArray);
+        }
+
+        //Convertimos el arraylist en array primitivo.
+        String [] primitivearray1 = arrayList1.toArray(new String[arrayList1.size()]);
+        String [] primitivearray2 = arrayList1.toArray(new String[arrayList1.size()]);
+        System.out.println(primitivearray1);
+        System.out.println(primitivearray2);
+
+        assertEquals(arrayList1,arrayList2);
+    }
+    @Test
+    public void objetos() throws InterruptedException {
+        Date objetoFecha1= new Date();
+        System.out.println(objetoFecha1.toString());
+        Date objetoFecha2= new Date();
+        System.out.println(objetoFecha2.toString());
+        //assertEquals(objetoFecha1.toString(),objetoFecha2.toString());
+        assertNotSame(objetoFecha1,objetoFecha2); //para objetos es con assertSame o NotSame
+
 
     }
 }
