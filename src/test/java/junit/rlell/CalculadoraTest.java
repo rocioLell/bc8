65858339a09aa1d441b1e0eb9f352e53c9090d13
Rocio Lell <rociobelenlell@hotmail.com>
2022-04-05 19:31:04 -0300

    ahora si

---
+++ src/test/java/junit/rlell/CalculadoraTest.java
@@ -0,0 +1,48 @@
+package junit.rlell;
+
+
+import junit.morona.Calculadora;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculadoraTest {
+    Calculadora objetoPrueba;
+    @BeforeClass
+    public static void beforeclass(){
+        System.out.println("beforeClass");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("before");
+        objetoPrueba = new Calculadora();//Precondicion
+
+    }
+    @Test
+    public void testSuma(){
+        System.out.println("testSuma");
+        int resultado = objetoPrueba.add(3,2);
+        int resulado_esperado = 5;
+        assertEquals(resulado_esperado,resultado);
+    }
+    @Test
+    public void testAnsSuma(){
+        System.out.println("testAnsSuma");
+        objetoPrueba.add(3,2);
+        int resultado = objetoPrueba.ans();
+        int resultado_esperado= 5;
+        assertEquals(resultado_esperado,resultado);
+
+
+    }
+    @After
+    public void after(){
+        System.out.println("after");
+
+    }
+
+}
