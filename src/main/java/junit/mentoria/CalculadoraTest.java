0704d8b77e3892468e98a188a08d7c54ce594893
Joel Ezequiel Vegas <Joel.Vegas@tsoftglobal.com>
2022-04-12 14:59:15 -0300

    jvegas

---
+++ src/main/java/junit/mentoria/CalculadoraTest.java
@@ -0,0 +1,47 @@
+package junit.mentoria;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CalculadoraTest {
+
+    Calculadora calculadoraTest;
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("BeforeClass");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("Before");
+        calculadoraTest = new Calculadora();
+    }
+
+    @Test
+    public void testSuma(){
+        System.out.println("testSuma");
+        int resultado = calculadoraTest.add(3,2);
+        int resultado_esperado = 5;
+        assertEquals(resultado_esperado, resultado);
+    }
+
+    @Test
+    public void testAnsSuma(){
+        System.out.println("testAnsSuma");
+        calculadoraTest.add(3,2);
+        int resultado = calculadoraTest.ans();
+        int resultado_esperado = 5;
+        assertEquals(resultado_esperado, resultado);
+    }
+
+    @After
+    public void after(){
+        System.out.println("after");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("afterClass");
+    }
+}
