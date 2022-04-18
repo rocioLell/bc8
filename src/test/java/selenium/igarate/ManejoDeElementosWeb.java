3be0a55c933580e439197cb74411ea55a87e719d
Israel Garate <israelgarate@gmail.com>
2022-04-18 17:41:43 -0400

    se sube archivo manejo de elementos web

--- src/test/java/selenium/igarate/ManejoDeElementosWeb.java
+++ src/test/java/selenium/igarate/ManejoDeElementosWeb.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import java.awt.*;
+
 import java.util.List;
 
 public class ManejoDeElementosWeb {
