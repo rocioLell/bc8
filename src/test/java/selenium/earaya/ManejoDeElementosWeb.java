901176b784f1e84be52a679bb45556297a03d828
fahelodev <fahelo.dev@gmail.com>
2022-04-18 13:07:49 -0400

    sesion 3. Selenium Base. WebElements

---
+++ src/test/java/selenium/earaya/ManejoDeElementosWeb.java
@@ -0,0 +1,88 @@
+package selenium.earaya;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class ManejoDeElementosWeb {
+
+    //atributos
+    public static WebDriver driver;
+    public By localizadorDropdown = By.xpath("//select[@id=\"dropdown\"]");
+    public By localizadorBtnEnabled = By.xpath("//a[text()=\"Enabled\"]");
+    public By localizadorBtnDownload = By.id("ui-id-4");
+    public By localizadorBtnPDF = By.xpath("//a[@href=\"/download/jqueryui/menu/menu.pdf\"]");
+    public By localizadoresCheckBoxes = By.xpath("//form[@id=\"checkboxes\"]");
+
+
+
+    @BeforeClass
+    public static void init(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setUp(){
+        //preparacion de Driver = Navegador
+        driver = new ChromeDriver(); //Chrome -> navegador
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void dropdown(){
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        WebElement WebElementDropdown = driver.findElement(localizadorDropdown);
+
+        //libreria Selenium -> webElement -> Select
+        Select manejoDropDown = new Select(WebElementDropdown);
+
+        //operar el select o dropdown
+        manejoDropDown.selectByValue("1");
+        manejoDropDown.selectByValue("2");
+        manejoDropDown.selectByVisibleText("Option 1");
+        manejoDropDown.selectByVisibleText("Option 2");
+
+    }
+
+    @Test
+    public void menuUi() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/jqueryui/menu#");
+        WebElement elementEnabled = driver.findElement(localizadorBtnEnabled);
+        elementEnabled.click();
+        Thread.sleep(1000);
+        WebElement elementDownloads = driver.findElement(localizadorBtnDownload);
+        elementDownloads.click();
+        Thread.sleep(1000);
+        WebElement elementPDF = driver.findElement(localizadorBtnPDF);
+        elementPDF.click();
+    }
+
+    @Test
+    public void checkbox() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        List<WebElement> checkboxes = driver.findElements(localizadoresCheckBoxes);
+        for (WebElement chk:checkboxes) {
+            chk.click();
+            Thread.sleep(1000);
+        }
+
+    }
+
+    @After
+    public void close(){
+        if(driver != null){
+            driver.close();
+        }
+    }
+
+}
