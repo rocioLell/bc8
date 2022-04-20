d1b6cb2f308c3264fa7de93eb76b17947a840b44
Emanuel Poggio <emanuel.poggio@hotmail.com>
2022-04-20 11:27:50 -0300

    Poggio EM, Actualizacion con trabajo grupal

--- src/test/java/selenium/poggioem/ATCChrome.java
+++ src/test/java/selenium/poggioem/ATCChrome.java
@@ -1,18 +1,10 @@
 package selenium.poggioem;
 
-/*
-1. Cargar Home
-2. Introducir "chiffon dress" en el campo de búsqueda
-3. Hacer clic en botón "Search"
-4. Esperar a que cargue la página de Resultados.
-5. Seleccionar el producto "Printed Chiffon Dress"
- */
-
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,9 +13,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ATCChrome {
 
     public static WebDriver driver;
+    //localizadores test1
     public By localizadorBusqueda = By.xpath("//input[@class=\"search_query form-control ac_input\"]");
     public By localizadorSearch = By.xpath("//*[@class=\"btn btn-default button-search\"]");
     public By localizadorPrintedChiffonDress = By.xpath("//img[@alt=\"Printed Chiffon Dress\"]");
+    public By localizadorDressName = By.xpath("//h1[@itemprop=\"name\"]");
+
+    //localizadores test5
+    public By localizadorBlouses = By.xpath("//div[@class=\"ac_results\"]");
+    public By localizadorBlousesNombre = By.xpath("//h1[@itemprop=\"name\"]");
+    public By localizadorBlouseModelo = By.xpath("//p[@id=\"product_reference\"]");
 
     @BeforeClass
     public static void init(){WebDriverManager.chromedriver().setup();}
@@ -33,39 +32,44 @@ public class ATCChrome {
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
     }
-    @Test
-    public void chromeSession() throws InterruptedException {
 
-        driver.get("http://automationpractice.com/index.php");
-        WebElement elementSRC = driver.findElement(localizadorBusqueda);
-        elementSRC.click();
-        Thread.sleep(1000);
-        elementSRC.sendKeys("Printed Chiffon Dress");
-        Thread.sleep(1000);
-        WebElement elementButton = driver.findElement(localizadorSearch);
-        elementButton.click();
-        Thread.sleep(1000);
-        WebElement elementDress = driver.findElement(localizadorPrintedChiffonDress);
-        elementDress.click();
-        Thread.sleep(3000);
-    }
- /*
+
     @Test
-    public void firefoxSession() throws InterruptedException {
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver = new FirefoxDriver();
+    public void ATC01() throws InterruptedException {
+        boolean resultadoT1 = false;
         driver.get("http://automationpractice.com/index.php");
         WebElement elementSRC = driver.findElement(localizadorBusqueda);
         elementSRC.click();
-        Thread.sleep(1000);
         elementSRC.sendKeys("Printed Chiffon Dress");
-        Thread.sleep(1000);
         WebElement elementButton = driver.findElement(localizadorSearch);
         elementButton.click();
-        Thread.sleep(1000);
         WebElement elementDress = driver.findElement(localizadorPrintedChiffonDress);
         elementDress.click();
-        Thread.sleep(3000);
-        driver.quit();
-    } */
+        Thread.sleep(2000);
+        WebElement elementName = driver.findElement(localizadorDressName);
+        if(elementName.getText().contains("Printed Chiffon Dress")){
+            resultadoT1 = true;
+        }
+        assertEquals(true,resultadoT1);
+    }
+
+
+    @Test
+    public void ATC05() throws InterruptedException {
+        boolean resultadoT5=false;
+        driver.get("http://automationpractice.com/index.php");
+        WebElement elementSRC = driver.findElement(localizadorBusqueda);
+        elementSRC.click();
+        elementSRC.sendKeys("blo");
+        Thread.sleep(2000);
+        WebElement blouses = driver.findElement(localizadorBlouses);
+        blouses.click();
+        WebElement blousesname = driver.findElement(localizadorBlousesNombre);
+        WebElement blousesmodel = driver.findElement(localizadorBlouseModelo);
+        if (blousesname.getText().contains("Blouse") && blousesmodel.getText().contains("Model demo_2")){
+            resultadoT5 = true;
+        }
+        assertEquals(true,resultadoT5);
+    }
+
 }
--- src/test/java/selenium/poggioem/ATCFirefox.java
+++ src/test/java/selenium/poggioem/ATCFirefox.java
@@ -1,4 +1,76 @@
 package selenium.poggioem;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
 
 public class ATCFirefox {
-}
+
+        public static WebDriver driver;
+        //localizadores test1
+        public By localizadorBusqueda = By.xpath("//input[@class=\"search_query form-control ac_input\"]");
+        public By localizadorSearch = By.xpath("//*[@class=\"btn btn-default button-search\"]");
+        public By localizadorPrintedChiffonDress = By.xpath("//img[@alt=\"Printed Chiffon Dress\"]");
+        public By localizadorDressName = By.xpath("//h1[@itemprop=\"name\"]");
+
+        //localizadores test5
+        public By localizadorBlouses = By.xpath("//div[@class=\"ac_results\"]");
+        public By localizadorBlousesNombre = By.xpath("//h1[@itemprop=\"name\"]");
+        public By localizadorBlouseModelo = By.xpath("//p[@id=\"product_reference\"]");
+
+        @BeforeClass
+        public static void init(){
+            WebDriverManager.firefoxdriver().setup();}
+        @Before
+        public void  setUp(){
+            driver = new FirefoxDriver();
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+        }
+
+        @Test
+        public void ATC01() throws InterruptedException {
+            boolean resultadoT1 = false;
+            driver.get("http://automationpractice.com/index.php");
+            WebElement elementSRC = driver.findElement(localizadorBusqueda);
+            elementSRC.click();
+            elementSRC.sendKeys("Printed Chiffon Dress");
+            WebElement elementButton = driver.findElement(localizadorSearch);
+            elementButton.click();
+            WebElement elementDress = driver.findElement(localizadorPrintedChiffonDress);
+            elementDress.click();
+            Thread.sleep(2000);
+            WebElement elementName = driver.findElement(localizadorDressName);
+            if(elementName.getText().contains("Printed Chiffon Dress")){
+                resultadoT1 = true;
+            }
+            assertEquals(true,resultadoT1);
+        }
+
+
+        @Test
+        public void ATC05() throws InterruptedException {
+            boolean resultadoT5=false;
+            driver.get("http://automationpractice.com/index.php");
+            WebElement elementSRC = driver.findElement(localizadorBusqueda);
+            elementSRC.click();
+            elementSRC.sendKeys("blo");
+            Thread.sleep(2000);
+            WebElement blouses = driver.findElement(localizadorBlouses);
+            blouses.click();
+            WebElement blousesname = driver.findElement(localizadorBlousesNombre);
+            WebElement blousesmodel = driver.findElement(localizadorBlouseModelo);
+            if (blousesname.getText().contains("Blouse") && blousesmodel.getText().contains("Model demo_2")){
+                resultadoT5 = true;
+            }
+            assertEquals(true,resultadoT5);
+        }
+
+    }
+
